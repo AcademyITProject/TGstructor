7b1e4d0e018ac478d155d3564742d90a500d2d32
Saveliy <miheev.saveliy.2002@gmail.com>
2023-04-21 20:38:15 +0800

    Валидация почты

--- back/back/settings.py
+++ back/back/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -41,7 +40,6 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
 ]
-
 
 # Application definition
 
@@ -90,7 +88,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "back.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -100,7 +97,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -120,7 +116,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -131,7 +126,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -159,3 +153,7 @@ CORS_ALLOW_HEADERS = [
     'Access-Control-Allow-Headers',
     'Access-Control-Allow-Origin',
 ]
+
+REST_FRAMEWORK = {
+    'EXCEPTION_HANDLER': 'rest_framework.views.exception_handler'
+}
--- back/user_api/validations.py
+++ back/user_api/validations.py
@@ -1,6 +1,10 @@
 from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.response import Response
+
 UserModel = get_user_model()
+
 
 def custom_validation(data):
     email = data['email'].strip()
@@ -8,7 +12,8 @@ def custom_validation(data):
     password = data['password'].strip()
     ##
     if not email or UserModel.objects.filter(email=email).exists():
-        raise ValidationError('choose another email')
+        # return ValidationError('Выберите другой email')
+        Response(status=status.HTTP_400_BAD_REQUEST)
     ##
     if not password or len(password) < 8:
         raise ValidationError('choose another password, min 8 characters')
@@ -24,11 +29,13 @@ def validate_email(data):
         raise ValidationError('an email is needed')
     return True
 
+
 def validate_username(data):
     username = data['username'].strip()
     if not username:
         raise ValidationError('choose another username')
     return True
+
 
 def validate_password(data):
     password = data['password'].strip()
